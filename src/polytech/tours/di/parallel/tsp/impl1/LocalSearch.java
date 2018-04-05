2f8a3d817db21201e6790bb4bf4caa05622c2c36
T0mat1 <thomas.rossipro@gmail.com>
2018-04-05 11:37:02 +0200

    localSearch update

--- src/polytech/tours/di/parallel/tsp/impl1/LocalSearch.java
+++ src/polytech/tours/di/parallel/tsp/impl1/LocalSearch.java
@@ -1,11 +1,40 @@
 package polytech.tours.di.parallel.tsp.impl1;
 
+import java.util.concurrent.Callable;
+
+import polytech.tours.di.parallel.tsp.Instance;
+import polytech.tours.di.parallel.tsp.Solution;
+import polytech.tours.di.parallel.tsp.TSPCostCalculator;
+
 /**
  * In these tasks, we relocate a city in every possible ways and keep only the best solution
  * @author Thomas Rossi Benoit Richard
  * @version %I%, %G%
  *
  */
-public class LocalSearch {
-
+public class LocalSearch implements Callable<Solution> {
+	
+	private Solution currentSolution;
+	private int currentCity;
+	private Instance instance;
+	
+	public LocalSearch(int city, Solution newSolution, Instance newInstance) {
+		currentSolution = newSolution;
+		currentCity = city;
+		instance = newInstance;
+	}
+	
+	@Override
+	public Solution call() {
+		Solution bestSolution = currentSolution.clone();
+		for (int city : currentSolution) {
+			if (currentCity != city)
+				currentSolution.relocate(currentCity, city);
+				currentSolution.setOF(TSPCostCalculator.calcOF(instance, currentSolution));
+			if (currentSolution.getOF()<bestSolution.getOF())
+				bestSolution = currentSolution.clone();
+		}
+		return bestSolution;
+	}
+	
 }
